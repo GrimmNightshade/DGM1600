2120e8b3d083ded6848d16eeb6cf3e329a9faadf
Unknown <wizardzrul@gmail.com>
2017-02-03 10:46:56 -0700

    Working With Math Operators
    In class working with Math Operators and my notes for them included inside the code.

---
+++ Scripting/Assets/MathOperators.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathOperators : MonoBehaviour {
+
+	public int addition = 12 + 4;
+	public int subtraction = 12 - 4;
+	public float division = 5/2f;
+	public int multiplication = 10 * 2;
+	public int remainder = 7 % 2;
+
+	//Note Floats without the f at the end will round it. 
+
+	// Use this for initialization
+
+	void Start () {
+		addition += 9;
+		addition += 3;
+		addition = 1;
+		// += Adds to a current total.
+		subtraction -= 10;
+
+		// -= Subtracts from value's total.
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
