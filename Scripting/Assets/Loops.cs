b58397b0f4a98e3460b925aca52240eaed0cc5ab
Unknown <wizardzrul@gmail.com>
2017-02-15 10:54:13 -0700

    Loops

---
+++ Scripting/Assets/Loops.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+	
+	int i=0;
+	public string[] colors;
+	//[This is an aray] elements, arays, and general scripting starts at zero. Not one.
+
+	// Use this for initialization
+	void Start () 
+	{
+		while (i < colors.Length)
+		{
+			print(colors[i]);
+			i++;
+		}
+
+		print ("Done");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
