b3d207ed211155b36ae2a0ca91aa1d04b5ad773a
Unknown <wizardzrul@gmail.com>
2017-02-23 20:47:33 -0700

    Learning Buttons, and Lists

---
+++ Scripting/Assets/AddtoInventoryList.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddtoInventoryList : MonoBehaviour {
+	public Lists Inventory;
+	public string myweapons;
+	// Use this for initialization
+	public void AddToList () {
+		Inventory.weaponsList.Add(myweapons);
+	}
+	
+
+}
