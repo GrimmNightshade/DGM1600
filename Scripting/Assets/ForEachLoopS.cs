460a906a0c9812b43d640c529ba9fe0adc6f035d
Unknown <wizardzrul@gmail.com>
2017-03-10 23:52:00 -0700

    Logic and Inventory

---
+++ Scripting/Assets/ForEachLoopS.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForEachLoopS : MonoBehaviour
+{
+
+    public List<GameObject> powerUpList;
+    public List<GameObject> cubeList;
+    public List<GameObject> sphereList;
+    public List<GameObject> capsuleList;
+
+    public void CheckData()
+    {
+
+        foreach (GameObject item in powerUpList)
+        {
+            switch (item.name)
+            {
+                case "Cube":
+                    cubeList.Add(item);
+                    break;
+                case "Sphere":
+                    cubeList.Add(item);
+                    break;
+                case "Capsule":
+                    cubeList.Add(item);
+                    break;
+
+            }
+        }
+    }
+}
+            
---
+++ Scripting/Assets/LogicOporatiors_2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicOporatiors_2 : MonoBehaviour {
+    public string Error;
+    public string HelpMe;
+	// Use this for initialization
+	public void Initiation() {
+		
+	}
+	
+	
+}
