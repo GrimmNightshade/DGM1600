7d151eecd55922ee6b9e1bf2c6fe6e593cc43419
Unknown <wizardzrul@gmail.com>
2017-04-07 14:09:16 -0600

    Edits

---
+++ Scripting/Assets/Other things/AddtoInventoryList.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddtoInventoryList : MonoBehaviour {
+    public Inventory I;
+    public string myweapons;
+
+    public void AddInventoryList() {
+        
+            I.weaponsList.Add(myweapons);
+    }
+}
