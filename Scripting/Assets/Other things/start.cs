0a5739f48acb221203c1f4c18603604272957042
Unknown <wizardzrul@gmail.com>
2017-04-07 14:14:07 -0600

    Yet another edit group.

---
+++ Scripting/Assets/Other things/start.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class start : MonoBehaviour {
+
+    public List<string> myCounter;
+    // Use this for initialization
+    
+    void Start()
+    {
+        myCounter.Add("One");
+    }
+
+}
